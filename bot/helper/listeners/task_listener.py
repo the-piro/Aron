443a0560224cca0c46b8fdd81603fa4d6c9ba805
5hojib <yesiamshojib@gmail.com>
2025-09-14 13:36:12 +0000

    format: auto-format code by ruff.
    Signed-off-by: 5hojib <yesiamshojib@gmail.com>

--- bot/helper/listeners/task_listener.py
+++ bot/helper/listeners/task_listener.py
@@ -478,7 +478,7 @@ class TaskListener(TaskConfig):
             await self.message._client.send_sticker(
                 self.message.chat.id,
                 "CAACAgIAAxkBAAEPX0ZoxsMuDlfSWesxGqfvGMb5khIt0gACJUkAArw3YUmtylzqifhguTYE",
-                reply_to_message_id=getattr(self.message, 'id', None),
+                reply_to_message_id=getattr(self.message, "id", None),
             )
             await send_message(self.message, done_msg)
         elif upload_service == "yt":
