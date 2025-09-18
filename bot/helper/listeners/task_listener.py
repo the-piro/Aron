eefc63ece2026a9e7ea2af37b406367aecefcdcb
the-piro <thezake0077@gmail.com>
2025-09-18 20:51:45 +0530

    Update task_listener.py

--- bot/helper/listeners/task_listener.py
+++ bot/helper/listeners/task_listener.py
@@ -429,7 +429,7 @@ class TaskListener(TaskConfig):
         ):
             await database.rm_complete_task(self.message.link)
         msg = f"<b>Name: </b><code>{escape(self.name)}</code>\n\n<b>Size: </b>{get_readable_file_size(self.size)}"
-        done_msg = f"{self.tag}\nYour task is complete\nPlease check your inbox."
+        done_msg = f"{self.tag}\nYour task is complete\nPlease check your inbox.\n\n<b>Name: </b><code>{escape(self.name)}</code>\n\n<b>Size: </b>{get_readable_file_size(self.size)}"
         LOGGER.info(f"Task Done: {self.name}")
 
         upload_service = (
