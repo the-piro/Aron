b351cdead6a700a0f58f48c9bf053545b9da83b6
the-piro <thezake0077@gmail.com>
2025-09-18 22:36:53 +0530

    Update task_listener.py

--- bot/helper/listeners/task_listener.py
+++ bot/helper/listeners/task_listener.py
@@ -683,15 +683,12 @@ class TaskListener(TaskConfig):
                 del task_dict[self.mid]
             count = len(task_dict)
         await self.remove_from_same_dir()
+        x_sticker = await self.message._client.send_sticker(self.message.chat.id, "CAACAgIAAxkBAAEPXc5oxbIjHClLKXF3ZCj21wGw3anlEQACNQwAAp1asUlhZqU29xC_PzYE")
+        await sleep(1)
         error_image_url = "https://telegra.ph/Extractor-Bot-09-18-2"
         caption = f"{self.tag} Download: {escape(str(error))}"
         x = await send_message(
             self.message, text=caption, photo=error_image_url, buttons=button
-        )
-        await sleep(1)
-        await self.message._client.send_sticker(
-            self.message.chat.id,
-            "CAACAgIAAxkBAAEPXc5oxbIjHClLKXF3ZCj21wGw3anlEQACNQwAAp1asUlhZqU29xC_PzYE",
         )
         create_task(auto_delete_message(x, time=300))
         if count == 0:
