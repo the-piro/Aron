cbd8f40ef1c71ea0e32279c1c4b78fb675d8ca1f
the-piro <thezake0077@gmail.com>
2025-09-18 14:07:40 +0530

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -295,7 +295,7 @@ async def update_status_message(sid, force=False):
                 obj.cancel()
                 del intervals["status"][sid]
             return
-        if not force and time() - status_dict[sid]["time"] < 3:
+        if not force and time() - status_dict[sid]["time"] < 10:
             return
         status_dict[sid]["time"] = time()
         page_no = status_dict[sid]["page_no"]
